0121ea81ff097f93560c7613a9ddcc7ae837ec19
Jakub Jezior <jakubjezior@gmail.com>
2026-03-06 08:55:29 +0100

    First commit

---
+++ src/test/java/lab/wzorce/projekt/ProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.wzorce.projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
